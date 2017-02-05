573e8132edd51d14e292281beeeed8c24ef193c4
KR <kong2350@users.noreply.github.com>
2017-02-05 15:52:59 -0800

    delete

--- conn/pdo_connect_server.php
+++ conn/pdo_connect_server.php
@@ -2,6 +2,16 @@
 // PDO connection
 try { 
 
+<<<<<<< Updated upstream
+=======
+	$conn = new PDO('mysql:host=localhost;dbname=bizsnap3_middle_earth','bizsnap3', 'v?n@Xm++');
+	$GLOBALS['connect'] = new PDO('mysql:host=localhost;dbname=bizsnap3_middle_earth','bizsnap3', 'v?n@Xm++');
+
+	
+	
+	//$conn = new PDO('mysql:host=localhost;dbname=bizsnap3_middle_earth','bizsnap3_chauc', 'cuahc2352Q');
+	//$GLOBALS['connect'] = new PDO('mysql:host=localhost;dbname=bizsnap3_middle_earth','bizsnap3_chau', 'cuahc2352Q');
+>>>>>>> Stashed changes
 	
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$GLOBALS['connect']->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
