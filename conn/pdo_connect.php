afb23fac283042fb698dba733e667143b77dfee5
kong2350 <kong2350@users.noreply.github.com>
2017-02-05 18:16:14 -0800

    Update pdo_connect.php

--- conn/pdo_connect.php
+++ conn/pdo_connect.php
@@ -2,8 +2,6 @@
 // PDO connection
 try { 
 
-	//$conn = new PDO('mysql:host=localhost;dbname=middle_earth','root', '22jPxhy1');
-	//$GLOBALS['connect'] = new PDO('mysql:host=localhost;dbname=middle_earth','root', '22jPxhy1');
         $conn = new PDO('mysql:host=localhost;dbname=middle_earth','root', 'root');
 	$GLOBALS['connect'] = new PDO('mysql:host=localhost;dbname=middle_earth','root', 'root');
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
