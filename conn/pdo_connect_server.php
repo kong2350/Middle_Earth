1580a22f400ee863b1a4c209efcb3a3a3a86fbea
KR <kong2350@users.noreply.github.com>
2017-02-05 17:43:02 -0800

    remove pass

--- conn/pdo_connect_server.php
+++ conn/pdo_connect_server.php
@@ -1,18 +1,9 @@
-<?php 
+<?php
 // PDO connection
-try { 
+try {
 
-<<<<<<< Updated upstream
-=======
-	$conn = new PDO('mysql:host=localhost;dbname=bizsnap3_middle_earth','bizsnap3', 'v?n@Xm++');
-	$GLOBALS['connect'] = new PDO('mysql:host=localhost;dbname=bizsnap3_middle_earth','bizsnap3', 'v?n@Xm++');
 
-	
-	
-	//$conn = new PDO('mysql:host=localhost;dbname=bizsnap3_middle_earth','bizsnap3_chauc', 'cuahc2352Q');
-	//$GLOBALS['connect'] = new PDO('mysql:host=localhost;dbname=bizsnap3_middle_earth','bizsnap3_chau', 'cuahc2352Q');
->>>>>>> Stashed changes
-	
+
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$GLOBALS['connect']->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
